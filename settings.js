7492654e9b8a7443f8403b4cebcd85a2c795d724
chathumal2006 <109399639+chathumal2006@users.noreply.github.com>
2022-07-16 14:31:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['948742378']
+global.premium = ['94764259590']
+global.ownernomer = '94764259590'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
